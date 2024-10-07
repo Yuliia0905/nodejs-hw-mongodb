2a6c05150438f3a2a4b0932f21cf472f1cfa912a
Yuliia_Harashchenko <yharashchenko@ukr.net>
2024-10-07 21:54:13 +0300

    add status 200

--- src/server.js
+++ src/server.js
@@ -21,6 +21,7 @@ export const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -34,6 +35,7 @@ export const setupServer = () => {
       return;
     }
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
